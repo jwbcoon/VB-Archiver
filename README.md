f5f58647790ec720abe7ddadd7caee803d93d987
Joe Coon <jwbcoon@gmail.com>
2023-03-11 17:57:01 -0800

    Added to readme and made a config file. Make YTDL work in command line.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Use youtube.dl to download Twitch VODs.
 YT.DL Github README: https://github.com/ytdl-org/youtube-dl/blob/master/README.md 
 
 Win32Com Page: https://pypi.org/project/pywin32/ 
+
+netrc: Read about for authentication https://stackoverflow.com/tags/.netrc/info
--- tv-archiver.py
+++ tv-archiver.py
@@ -1,9 +1,8 @@
 from __future__ import unicode_literals
 import youtube_dl
-import win32com
+#import win32com
 
-ydl_opts = {}
+ydl_opts = { '--config-location .\\config.txt' }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
 
-#print("Hello")
