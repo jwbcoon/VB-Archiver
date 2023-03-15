444087ae807dd68d297992a704f109f2b175c9cc
Joe Coon <jwbcoon@gmail.com>
2023-03-14 18:07:55 -0700

    updating setup_vba.sh to add venv

--- setup_vba.sh
+++ setup_vba.sh
@@ -1,5 +1,7 @@
 # Be sure this script is within the top-level of the vb-archiver project folder
 echo "Installing prerequisite files..."
+mkdir venv
+python -m venv ./venv/
 pip install git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl
 mkdir dldest
 mkdir -p ./gui/node_modules/
