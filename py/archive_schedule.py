a98e0dedcd3c8a52c1e08e8f9bc19221d16e7e70
Joe Coon <jwbcoon@gmail.com>
2023-03-18 14:32:43 -0700

    Added logging, trying to find why archiver.py is not being run by the Windows Task Scheduler.

--- py/archive_schedule.py
+++ py/archive_schedule.py
@@ -1,13 +1,17 @@
 import subprocess
+import logging
 import archiver
 import os
 
+logging.basicConfig(filename='logs/archive_schedule.log',
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s: %(message)s')
+
 # Make a video archive schedule
 def make_schedule(frequency, modifier, day):
-    interp_path = '\''.join(['', os.path.abspath(os.environ['_']),''])
+    interp_path = os.path.abspath(os.environ['_'])
     dir_path = os.path.abspath(archiver.__file__)
     task_with_args = ' '.join([interp_path, dir_path])
-    task_with_args = '\"'.join(['', task_with_args, '']) # maybe unnecessary?
     task_name = 'vb_archiver'
     task_command = ['schtasks.exe',
                     '/Create',
@@ -16,21 +20,28 @@ def make_schedule(frequency, modifier, day):
                     '/MO', modifier,
                     #'/D', day,
                     '/TN', task_name,
-                    '/TR', task_with_args]
-    subprocess.run(task_command)
+                    '/TR', task_with_args,
+                    '/RL', 'HIGHEST']
+    
+    logging.info('Creating vb_archiver task ... ')
+    logging.info('\n'.join(['', ' '.join(task_command)]))
+    taskproc = subprocess.run(task_command)
+    logging.info('stdout: {0}'.format(taskproc.stdout))
+    logging.error('stderr: {0}'.format(taskproc.stderr))
 
 # Export data to be used in another file
 
 def get():
-    output_path = './dldest/filenames.txt'
+    output_path = os.path.abspath('../dldest/filenames.txt')
     url = 'https://www.twitch.tv/northbaysmash/videos?filter=highlights&sort=time'
     args = [
         'youtube-dl',
         url,
-        '--config-location', os.path.abspath('./py/config.txt'),
+        '--config-location', os.path.abspath('./config.txt'),
         '--yes-playlist']
+    logging.info('Sending dldest path: %(output_path)s')
     return {'args': args, 'output_path': output_path}
 
 
 if (__name__ == '__main__'):
-    make_schedule('MINUTE', '5', '*')
+    make_schedule('MINUTE', '1', '*')
--- py/archiver.py
+++ py/archiver.py
@@ -1,10 +1,18 @@
 import subprocess
-import arch_schedule as schedule
+import logging
+import archive_schedule as schedule
 
-# runs the youtube-dl shell commands and redirects output to dldest
-def scan():
+logging.basicConfig(filename='logs/archiver.log',
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s: %(message)s')
+
+# runs the youtube-dl shell commands and redirects output to dldest/filenames.txt
+def main():
+    logging.info('Running ytdl ... ')
     with open(schedule.get()['output_path'], 'w') as outfile:
-        subprocess.run(schedule.get()['args'], stdout=outfile)
+        dlproc = subprocess.run(schedule.get()['args'], stdout=outfile)
+        logging.info('stdout: {0}'.format(dlproc.stdout))
+        logging.error('stderr: {0}'.format(dlproc.stderr))
 
 if (__name__ == '__main__'):
-    scan()
+    main()
