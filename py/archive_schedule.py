837757f44d121b779985b64dd3161429b93b9485
Joe Coon <jwbcoon@gmail.com>
2023-03-19 14:35:01 -0700

    Trying out XML config for schtasks

--- py/archive_schedule.py
+++ py/archive_schedule.py
@@ -1,6 +1,5 @@
 import subprocess
 import logging
-import archiver
 import os
 
 log_file = os.path.abspath(
@@ -11,19 +10,15 @@ logging.basicConfig(filename=log_file,
 
 # Make a video archive schedule
 def make_schedule(frequency, modifier, day):
-    interp_path = os.path.abspath(os.environ['_'])
-    dir_path = os.path.abspath(archiver.__file__)
-    task_with_args = ' '.join([interp_path, dir_path])
-    task_name = 'vb_archiver'
+    #interp_path = os.path.abspath(os.environ['_'])
+    #dir_path = os.path.abspath(archiver.__file__)
+    #task_with_args = ' '.join([interp_path, dir_path])
+    #task_name = 'vb_archiver'
     task_command = ['schtasks.exe',
                     '/Create',
-                    '/RU', 'SYSTEM',
-                    '/SC', frequency,
-                    '/MO', modifier,
-                    #'/D', day,
-                    '/TN', task_name,
-                    '/TR', task_with_args,
-                    '/RL', 'HIGHEST']
+                    '/RU', 'System',
+                    '/TN', 'vb_archiver',
+                    '/XML', './py/vb_archiver.xml']
     
     logging.info('Creating vb_archiver task ... ')
     logging.info((' '.join(task_command)))
@@ -33,18 +28,20 @@ def make_schedule(frequency, modifier, day):
 
 # Export data to be used in another file
 
-def get():
+def contents():
     output_path = os.path.abspath(
-        os.path.join(os.path.dirname(archiver.__file__), '../dldest/filenames.txt') )
+        os.path.join(
+            os.path.split(os.path.dirname(__file__))[0], 
+            'dldest/filenames.txt') )
     url = 'https://www.twitch.tv/northbaysmash/videos?filter=highlights&sort=time'
     args = [
         'youtube-dl',
         url,
-        '--config-location', os.path.abspath('./config.txt'),
+        '--config-location', os.path.abspath(os.path.join(os.path.dirname(__file__), 'config.txt')),
         '--yes-playlist']
     logging.info('Sending dldest path: {0}'.format(output_path))
     return {'args': args, 'output_path': output_path}
 
 
 if (__name__ == '__main__'):
-    make_schedule('MINUTE', '1', '*')
+    make_schedule('MINUTE', '2', '*')
--- py/archiver.py
+++ py/archiver.py
@@ -1,6 +1,7 @@
 import subprocess
 import logging
 import archive_schedule as schedule
+from inspect import currentframe
 import os
 
 log_file = os.path.abspath(
@@ -12,10 +13,15 @@ logging.basicConfig(filename=log_file,
 # runs the youtube-dl shell commands and redirects output to dldest/filenames.txt
 def main():
     logging.info('Running ytdl ... ')
-    with open(schedule.get()['output_path'], 'w') as outfile:
-        dlproc = subprocess.run(schedule.get()['args'], stdout=outfile)
-        logging.info('stdout: {0}'.format(dlproc.stdout))
+    sched_contents = schedule.contents()
+    with open(sched_contents['output_path'], 'w') as outfile:
+        dlproc = subprocess.run(sched_contents['args'], stdout=outfile)
+        logging.info('output_path: {0}'.format(outfile))
         logging.error('stderr: {0}'.format(dlproc.stderr))
 
-if (__name__ == '__main__'):
-    main()
+if (__name__ == '__main__'): #unsure if name should be main or archiver
+    logging.info('__name__ == {0}, line {1}'.format(__name__, currentframe().f_lineno))
+    try:
+        main()
+    except Exception as e:
+        logging.error('Error starting ytdl; line {0}: \n{1}'.format(currentframe().f_lineno, e))
