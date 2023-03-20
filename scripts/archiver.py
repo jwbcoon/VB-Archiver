9e38dba89f6068d13db2a75be817aeb82e05fb05
Joe Coon <jwbcoon@gmail.com>
2023-03-20 02:33:35 -0700

    Adding powershell scripting to see if it will enable window's task scheduler to execute the archiver tasks.

--- scripts/archiver.py
+++ scripts/archiver.py
@@ -1,6 +1,7 @@
 import subprocess
 import logging
 import archive_schedule as schedule
+from inspect import currentframe
 import os
 
 log_file = os.path.abspath(
@@ -12,10 +13,15 @@ logging.basicConfig(filename=log_file,
 # runs the youtube-dl shell commands and redirects output to dldest/filenames.txt
 def main():
     logging.info('Running ytdl ... ')
-    with open(schedule.get()['output_path'], 'w') as outfile:
-        dlproc = subprocess.run(schedule.get()['args'], stdout=outfile)
-        logging.info('stdout: {0}'.format(dlproc.stdout))
+    sched_contents = schedule.contents()
+    with open(sched_contents['output_path'], 'w') as outfile:
+        dlproc = subprocess.run(sched_contents['args'], stdout=outfile)
         logging.error('stderr: {0}'.format(dlproc.stderr))
 
 if (__name__ == '__main__'):
-    main()
+    logging.info('Starting archiver. __name__ == {0}'.format(__name__))
+    try:
+        main()
+    except Exception as e:
+        logging.error('Failed to start archiver.py; line {0}\n{1}'
+                      .format(currentframe().f_lineno, e))
