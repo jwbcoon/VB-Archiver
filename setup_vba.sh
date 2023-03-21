9c16451d68d67b98353362322577ff686a386c64
Joe Coon <jwbcoon@gmail.com>
2023-03-21 12:45:00 -0700

    forgot to add pip install dicttoxml

--- setup_vba.sh
+++ setup_vba.sh
@@ -5,6 +5,7 @@ python -m venv ./.venv/
 mkdir .vscode
 echo "{\"python.defaultInterpreterPath\":\"\${workspaceFolder}/.venv/Scripts/python.exe\",\"python.pipenvPath\":\"\${workspaceFolder}/.venv/Scripts/pip.exe\"}" > ./.vscode/settings.json
 pip install git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl
+pip install dicttoxml
 mkdir dldest
 mkdir -p ./gui/node_modules/
 npm install --prefix ./gui/node_modules/
