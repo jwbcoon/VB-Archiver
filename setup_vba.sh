9d91c4ed5266e76c911014584018cdca4da4b260
Joe Coon <jwbcoon@gmail.com>
2023-03-22 03:18:35 -0700

    Keep fixing the setup_vba script

--- setup_vba.sh
+++ setup_vba.sh
@@ -1,12 +1,12 @@
 # Be sure this script is within the top-level of the vb-archiver project folder
-echo "Making Directories ... "
-mkdir .venv
-python -m venv ./.venv/
-mkdir .vscode
-mkdir dldest
-mkdir -p ./gui/node_modules/
-echo "{\"python.defaultInterpreterPath\":\"\${workspaceFolder}/.venv/Scripts/python.exe\",\"python.pipenvPath\":\"\${workspaceFolder}/.venv/Scripts/pip.exe\"}" > ./.vscode/settings.json
-exec $SHELL
+echo "Making Directories ... " && \
+mkdir .venv && \
+python -m venv ./.venv/ && \
+mkdir .vscode && \
+mkdir dldest && \
+mkdir -p ./gui/node_modules/ && \
+echo "{\"python.defaultInterpreterPath\":\"\${workspaceFolder}/.venv/Scripts/python.exe\",\"python.pipenvPath\":\"\${workspaceFolder}/.venv/Scripts/pip.exe\"}" > ./.vscode/settings.json && \
+exec $SHELL && source setup_vba.sh
 echo "Installing prerequisite files ... "
 pip install git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl
 pip install dicttoxml
