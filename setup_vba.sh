9da83714107aa609fe013a0c80c73377170099cf
Joe Coon <jwbcoon@gmail.com>
2023-03-22 03:00:58 -0700

    Added shell restart to setup_vba.sh

--- setup_vba.sh
+++ setup_vba.sh
@@ -1,11 +1,13 @@
 # Be sure this script is within the top-level of the vb-archiver project folder
-echo "Installing prerequisite files..."
+echo "Making Directories ... "
 mkdir .venv
 python -m venv ./.venv/
 mkdir .vscode
-echo "{\"python.defaultInterpreterPath\":\"\${workspaceFolder}/.venv/Scripts/python.exe\",\"python.pipenvPath\":\"\${workspaceFolder}/.venv/Scripts/pip.exe\"}" > ./.vscode/settings.json
-pip install git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl
-pip install dicttoxml
 mkdir dldest
 mkdir -p ./gui/node_modules/
+echo "{\"python.defaultInterpreterPath\":\"\${workspaceFolder}/.venv/Scripts/python.exe\",\"python.pipenvPath\":\"\${workspaceFolder}/.venv/Scripts/pip.exe\"}" > ./.vscode/settings.json
+exec $SHELL
+echo "Installing prerequisite files ... "
+pip install git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl
+pip install dicttoxml
 npm install --prefix ./gui/node_modules/
