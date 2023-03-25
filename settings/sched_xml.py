0c41607df9c3e406ea6b09247ee8d916f2a708c0
Joe Coon <jwbcoon@gmail.com>
2023-03-25 15:14:23 -0700

    schedule_config class successfully compiles as a subclass of tuple.

--- settings/sched_xml.py
+++ settings/sched_xml.py
@@ -1,10 +1,28 @@
 import dicttoxml as dtx
 from init import archive_schedule as sched
 
-# Template for generating xml files
+# TODO: Template for generating xml files according to the task scheduler schema
 xml_schema = {
-    
+    'task': {
+        'principal-key': {
+            'selector': None, # selector field
+            'field': None # user(?) id field
+        },
+        'context-key-ref': {
+            'selector': None, # selector field
+            'field': None, # context field
+        },
+        'unique-id': {
+            'selector': None, # selector field related to triggers
+            'field': None # unique event id
+        }
+    }
 }
 
+def generate(value):
+    # TODO: code that confirms the XML is valid to the task scheduler schema
+    return dtx.dicttoxml(value)
+
 # Generate XML files for schtasks.exe to run
-xml = dtx.dicttoxml(sched.profile())
+if (__name__ == '__main__'):
+    xml = generate(sched.schedule_config().sched_opts)
