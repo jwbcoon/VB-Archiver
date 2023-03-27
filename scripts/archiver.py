8561751e24f34e9576a83caaa888c489c6ce19d0
jwbcoon <jwbcoon@gmail.com>
2023-03-26 22:38:12 -0700

    Moved management of XML and YDL schemas to new file, vba_schedule.py Also refactored schedule_config class to become vba_schedule class in the new file. vba_schedule objects now have a URL property, associating objects with a URL to archive videos from.
    Made large strides on implementing XML Schema for generating XML and
    made YDL schema for validating YDL settings before creating tasks.

--- scripts/archiver.py
+++ scripts/archiver.py
@@ -1,9 +1,10 @@
 import subprocess
 import archive_schedule as schedule
+from vba_schedule import vba_schedule as vbas
 
 # runs the youtube-dl shell commands and redirects output to dldest/filenames.txt
-def main():
-    sched_contents = schedule.current.contents()
+def main(vb: vbas):
+    sched_contents = schedule.contents(vb)
     with open(sched_contents['output_path'], 'w') as outfile:
         subprocess.run(sched_contents['args'], stdout=outfile)
 
